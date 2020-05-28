5ce67e32fb18a4c82401b141fe6fe2d2cd8839d7
Rob Wallator <rob@rawimpressions.ca>
2020-05-28 02:14:03 +0000

    countletters

---
+++ countLetters.js
@@ -0,0 +1,25 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(word) {
+  let numLetter = {};
+  for (const letter of word) {
+    if (letter === ' ') {
+      continue;
+    } else if (!numLetter[letter]) {
+      numLetter[letter] = 1;
+  
+    } else {
+      numLetter[letter] += 1;
+    }
+    
+  } return numLetter;
+};
+
+console.log(countLetters('lighthouse labs is the place to be'));
