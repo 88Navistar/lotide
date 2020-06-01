be2aad05093ae36bb29cfe104114fbf9176ed360
Rob Wallator <rob@rawimpressions.ca>
2020-06-01 23:50:25 +0000

    chai test added

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,20 @@
-
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
+describe("#middle", () => {
+  it("should return the middle number 4", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]);
+  });
+  it("should return an empty array", () => {
+    assert.deepEqual(middle([2]), []);
+  });
+  it("should return middle two numbers 2 & 3", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+  it("should return an empty array", () => {
+    assert.deepEqual(middle([2, 2]), []);
+  });
 
-assertArraysEqual((middle([1, 2, 3, 4, 5, 6, 7]), [4])); // => []
-eqArrays(middle([1, 2, 3, 4, 5, 6, 7]), [4]);
-middle([1, 2]); // => []
+
+});
+
