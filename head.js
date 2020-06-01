812c1dabf4d684a3ef7712dc0be998bcc8f9c0c1
Rob Wallator <rob@rawimpressions.ca>
2020-06-01 20:34:48 +0000

    refactored

--- head.js
+++ head.js
@@ -1,19 +1,6 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
+const head = function (list) {
+  return list[0];
 };
+head('');
 
-let head = function (list) {
-  return list[0]
-}
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-// TEST CODE
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
-// assertEqual("Bootcamp", "Bootcamp");
-// assertEqual(1, 2);
+module.exports = head;
