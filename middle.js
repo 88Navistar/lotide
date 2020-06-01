802d7f5c9baa8de89a8f9db4b585863b5d0d3a36
Rob Wallator <rob@rawimpressions.ca>
2020-06-01 23:24:12 +0000

    add two

--- middle.js
+++ middle.js
@@ -1,24 +1,3 @@
-// FUNCTION IMPLEMENTATION
-const eqArrays = function(xArray, yArray) {
-  if (xArray.length !== yArray.length) {
-    return false;
-  }
-  for (let i = 0; i < xArray.length; i++) {
-    if (xArray[i] !== yArray[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(arrayA, arrayB) {
-  if (eqArrays(arrayA, arrayB)) {
-    console.log(`✅✅✅Assertion Passed: ${arrayA} === ${arrayB}`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: ${arrayA} !== ${arrayB}`);
-  }
-};
-
 const middle = function(array) {
   const midArray = Math.floor((array.length / 2));
   if (array.length <= 2) {
@@ -30,43 +9,4 @@ const middle = function(array) {
   }
 };
 
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
-
-// // TEST/ASSERTION FUNCTIONS
-// const eqArrays = function(...) {
-//   //...
-// }
-
-// const assertArraysEqual = function(actual, expected) {
-//   //...
-// }
-
-// // ACTUAL FUNCTION
-// const middle = function(array) {
-//   //...
-// }
-
-
-// Part one and two, not needed for middle.js
-// TEST CODE
-// ...
-
-// assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
-// assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
-
-// assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
-// assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
-
-// TEST CODE for part 2
-//assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-
-// TEST CODE from assert Equals part 1
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
-// assertEqual("Bootcamp", "Bootcamp");
-// assertEqual(1, 2);
+module.exports = middle;
