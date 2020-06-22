f98032533b95c6fc073e268c91655fb26026cfe9
Rob Wallator <rob@rawimpressions.ca>
2020-06-22 22:50:32 +0000

    june 22 redo

--- head.js
+++ head.js
@@ -1,6 +1,17 @@
-
-const head = function (list) {
-  return list[0];
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
 };
 
-module.exports = head;
+const head = function (list) {
+  
+  return list[0];
+};
+console.log(head(['w']))
+assertEqual(head(['5,6,7']), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+//module.exports = head;
