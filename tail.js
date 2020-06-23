bd597f0e97774a09261b143ffa7b20c2b5105527
Rob Wallator <rob@rawimpressions.ca>
2020-06-23 01:02:51 +0000

    update june 22

--- tail.js
+++ tail.js
@@ -1,6 +1,16 @@
-let tail = function(words) {
-  return words.shift;
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
 };
 
-module.exports = tail;
+const tail = (array) => array.slice(1);
+
+const array = ["Yo Yo", "Lighthouse", "Labs"];
+console.log(tail(array))
+//tail(words); // no need to capture the return value since we are not checking it
+assertEqual(array.length, 3); // original array should still have 3 elements!
+//module.exports = tail;
 
