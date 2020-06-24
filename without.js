66ffa382dfa56a0321683a891bec83bd3bf4c66c
Rob Wallator <rob@rawimpressions.ca>
2020-06-24 01:10:47 +0000

    june 23 update

--- without.js
+++ without.js
@@ -22,8 +22,8 @@ const eqArrays = function(xArray, yArray) {
 const without = (source, itemToRemove) => {
   let newArray;
   itemToRemove.forEach(item => {
-    newArray = source.filter(function(numbers) {
-      return numbers !== item;
+    newArray = source.filter(function(el) {
+      return el !== item;
     });
   });
   return newArray;
@@ -31,24 +31,10 @@ const without = (source, itemToRemove) => {
 };
 
 without([1, 2, 3], [1]) // => [2, 3]
-without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
+console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
 
 const words = ["hello", "world", "lighthouse"];
 without(words, ["lighthouse"]); // no need to capture return value for this test case
 // Make sure the original array was not altered by the without function
 assertArraysEqual(words, ["hello", "world", "lighthouse"]);
 
-// assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
-// assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
-
-// assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
-// assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
-
-// TEST CODE for part 2
-//assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-
-// TEST CODE from assert Equals part 1
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
-// assertEqual("Bootcamp", "Bootcamp");
-// assertEqual(1, 2);
