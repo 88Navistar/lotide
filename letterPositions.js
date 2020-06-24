e626abe924365ecd99a313e78f98d50581aea10b
Rob Wallator <rob@rawimpressions.ca>
2020-06-24 22:04:36 +0000

    clean up June 24

--- letterPositions.js
+++ letterPositions.js
@@ -37,16 +37,4 @@ const letterPositions = function(sentence) {
 console.log(letterPositions('lighthouse labs is the place to be'))
 //console.log(countLetters('lighthouse labs is the place to be'));
 
-//assertArraysEqual(letterPositions("hello").e, [1]);
-// { 
-//   l: [0],
-//   i: [1, 11],
-//   g: [2],
-//   h: [3, 5, 15, 18],
-//   t: [4, 14],
-//   o: [6, 19],
-//   u: [7, 20],
-//   s: [8, 21],
-//   e: [9, 16, 22],
-//   n: [12]
-// }
+assertArraysEqual(letterPositions("hello").e, [1]);
