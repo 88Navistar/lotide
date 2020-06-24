e45f6af6813e1655cdbd4ed3100f073cb3ca3f6e
Rob Wallator <rob@rawimpressions.ca>
2020-06-24 19:22:32 +0000

    updated and tested

--- middle.js
+++ middle.js
@@ -1,12 +1,29 @@
+const eqArrays = require('./eqArrays');
+
 const middle = function(array) {
-  const midArray = Math.floor((array.length / 2));
+  let midArray = Math.floor((array.length / 2));
+  //console.log(midArray);
   if (array.length <= 2) {
     return [];
   } else if (array.length % 2 !== 0) {
-    return array[midArray];
+    return [array[midArray]];
   } else {
     return ([array[midArray - 1], array[midArray]]);
   }
 };
+// middle([1, 2, 3, 4, 5]);
+// middle([1, 2, 3, 4, 5, 6])
+console.log(middle([1, 2]));
+console.log(middle([1, 2, 3, 4, 5]));
+console.log(middle([1, 2, 3, 4, 5, 6]));
 
-module.exports = middle;
+const assertArraysEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqArrays(actual, expected)) {
+    console.log(`✅Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`❗️Assertion Failed:  ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
+
+console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]));
