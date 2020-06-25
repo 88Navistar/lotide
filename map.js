041e7f4e489f79b3d6b448567c8973dafde7a493
Rob Wallator <rob@rawimpressions.ca>
2020-06-25 22:54:52 +0000

    added new assertion tests

--- map.js
+++ map.js
@@ -19,6 +19,7 @@ const eqArrays = function(xArray, yArray) {
 };
 
 const words = ["ground", "control", "to", "major", "tom"];
+
 const map = function(array, callback) {
   const results = [];
   for (let item of array) {
@@ -27,7 +28,10 @@ const map = function(array, callback) {
   return results;
 }
 
-
 const results1 = map(words, word => word[0]);
+const results2 = map(words, word => word + ' ' + 88)
+const results3 = map(words, word => word.length)
 console.log(results1);
-
+assertArraysEqual(results1, [ 'g', 'c', 't', 'm', 't' ]);
+assertArraysEqual(results2, ["ground 88", "control 88", "to 88", "major 88", "tom 88"]);
+assertArraysEqual(results3, [6, 7, 2, 5, 3]);
