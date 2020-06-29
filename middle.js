c719f2e6b29dff88aa1870758a08ba9051582377
Rob Wallator <rob@rawimpressions.ca>
2020-06-29 19:32:35 +0000

    CleanUp

--- middle.js
+++ middle.js
@@ -1,4 +1,3 @@
-const eqArrays = require('./eqArrays');
 
 const middle = function(array) {
   let midArray = Math.floor((array.length / 2));
@@ -11,19 +10,6 @@ const middle = function(array) {
     return ([array[midArray - 1], array[midArray]]);
   }
 };
-// middle([1, 2, 3, 4, 5]);
-// middle([1, 2, 3, 4, 5, 6])
-console.log(middle([1, 2]));
-console.log(middle([1, 2, 3, 4, 5]));
-console.log(middle([1, 2, 3, 4, 5, 6]));
 
-const assertArraysEqual = function(actual, expected) {
-  const inspect = require('util').inspect;
-  if (eqArrays(actual, expected)) {
-    console.log(`✅Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
-  } else {
-    console.log(`❗️Assertion Failed:  ${inspect(actual)} !== ${inspect(expected)}`);
-  }
-};
+module.exports = middle;
 
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]));
