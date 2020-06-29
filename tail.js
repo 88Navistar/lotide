3342864cfc632031b154b99671f77b6cdc831a77
Rob Wallator <rob@rawimpressions.ca>
2020-06-29 19:31:32 +0000

    CleanUp

--- tail.js
+++ tail.js
@@ -1,17 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = (array) => array.slice(1);
 
-const array = ["Yo Yo", "Lighthouse", "Labs"];
-console.log(tail(array))
-assertEqual(array.length, 3); // original array should still have 3 elements!
+module.exports = tail;
 
-
-//module.exports = tail;
-
+//const array = ["Yo Yo", "Lighthouse", "Labs"];
+// console.log(tail(array))
+// assertEqual(array.length, 3); // original array should still have 3 elements!
