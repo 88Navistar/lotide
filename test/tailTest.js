35892a092bf0f46718981b5752675e3f1a63116d
Rob Wallator <rob@rawimpressions.ca>
2020-06-29 19:28:48 +0000

    CleanUp

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,14 @@
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3);
+describe("#tail", () => {
+  it("returns 2, 3 for [1, 2, 3]", () => {
+    assert.strictEqual(tail([1, 2, 3]), 2, 3);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(tail(['5']), '5'); 
+  });
+});
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3);
