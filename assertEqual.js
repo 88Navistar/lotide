9cfad98953942b4f447405e4caed96f407bfb07a
Rob Wallator <rob@rawimpressions.ca>
2020-06-29 19:30:59 +0000

    CleanUp

--- assertEqual.js
+++ assertEqual.js
@@ -6,8 +6,5 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-console.log(assertEqual('rob', 'rob'));
-console.log(assertEqual(1, 2));
-
 
 module.exports = assertEqual;
