d823467ff2a9fb18971df7d8767c1dd8701b4d36
Rob Wallator <rob@rawimpressions.ca>
2020-06-30 02:57:56 +0000

    Final update with stretch files

--- index.js
+++ index.js
@@ -2,15 +2,28 @@
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
+const flatten = require('./flatten');
+const countOnly = require('./countOnly');
+const letterPositions = require('./letterPositions');
+const findKeyByValue = require('./findKeyByValue')
 const eqArrays = require('./eqArrays');
 const assertArraysEqual = require('./assertArraysEqual');
 const assertEqual = require('./assertEqual');
+const eqObjects = require('./eqObjects');
+const eqArrays = require('./eqArrays');
+
 
 module.exports = {
   head,
   tail,
   middle,
+  flatten,
   eqArrays,
   assertArraysEqual,
-  assertEqual
+  assertEqual,
+  countOnly,
+  letterPositions,
+  findKeyByValue,
+  eqObjects,
+  eqArrays
 };
