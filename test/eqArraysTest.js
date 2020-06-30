923fc640fe8944a8055bff3a81453bc99318d8ab
Rob Wallator <rob@rawimpressions.ca>
2020-06-30 02:59:04 +0000

    Test added

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,7 +1,16 @@
 const eqArrays = require('../eqArrays');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+describe("#eqArrays", () => {
+  it("returns true", () => {
+    assert.deepEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
+  });
+
+  it("returns false", () => {
+    assert.deepEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); 
+  });
+});
+eqArrays([1, 2, 3], [1, 2, 3]); // => true
 eqArrays([1, 2, 3], [3, 2, 1]); // => false
 
 // eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
