cf2339fcfc8477429ad4fa0dbdfad89812df9c85
Rob Wallator <rob@rawimpressions.ca>
2020-06-30 02:32:28 +0000

    refactor for test

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,25 +1,16 @@
 // FUNCTION IMPLEMENTATION
-  function findKeyByValue(object, value) {
+  const findKeyByValue = (object, value) => {
     return Object.keys(object).find(key => object[key] === value);
-  }
+  };
 //CLASSIC For Loop
-const findKeyByValue = (obj, value) => {
-  for (let key in obj) {
-    console.log(key);
-    if (obj[key] === value) {
-      return key
-    } else {
-      return undefined
-    }
-  } 
-};
-
-const bestTVShowsByGenre = { 
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
-console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-console.log(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+// const findKeyByValue = (obj, value) => {
+//   for (let key in obj) {
+//     console.log(key);
+//     if (obj[key] === value) {
+//       return key
+//     } else {
+//       return undefined
+//     }
+//   } 
+// };
+module.exports = findKeyByValue;
