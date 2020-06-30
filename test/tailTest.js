cf75859cb53e4b34fb8a1515980e681b5ef8ffe0
Rob Wallator <rob@rawimpressions.ca>
2020-06-30 01:13:08 +0000

    small update

--- test/tailTest.js
+++ test/tailTest.js
@@ -2,11 +2,11 @@ const tail = require('../tail');
 const assert = require('chai').assert;
 
 describe("#tail", () => {
-  it("returns 2, 3 for [1, 2, 3]", () => {
-    assert.strictEqual(tail([1, 2, 3]), 2, 3);
+  it("returns 2, 3, 4 for [1, 2, 3, 4]", () => {
+    assert.deepEqual(tail([1, 2, 3, 4]), [2, 3, 4]);
   });
-  it("returns '5' for ['5']", () => {
-    assert.strictEqual(tail(['5']), '5'); 
+  it("returns empty array for ['5']", () => {
+    assert.deepEqual(tail(['5']), []); 
   });
 });
 // const words = ["Yo Yo", "Lighthouse", "Labs"];
