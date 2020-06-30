5da5b75ba2fc922df96b7448e5c3512f46efe956
Rob Wallator <rob@rawimpressions.ca>
2020-06-30 02:58:25 +0000

    package.json

--- letterPositions.js
+++ letterPositions.js
@@ -1,24 +1,3 @@
-// FUNCTION IMPLEMENTATION
-const assertArraysEqual = function(arrayA, arrayB) {
-  if (eqArrays(arrayA, arrayB)) {
-    console.log(`✅✅✅Assertion Passed: ${arrayA} === ${arrayB}`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: ${arrayA} !== ${arrayB}`);
-  }
-};
-
-const eqArrays = function(xArray, yArray) {
-  if (xArray.length !== yArray.length) {
-    return false
-  }
-  for (let i = 0; i < xArray.length; i++) {
-    if (xArray[i] !== yArray[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
 const letterPositions = function(sentence) {
   const results = {};
     for (let i = 0; i < sentence.length; i++) {
@@ -34,7 +13,5 @@ const letterPositions = function(sentence) {
       
     } return results;
   };
-console.log(letterPositions('lighthouse labs is the place to be'))
-//console.log(countLetters('lighthouse labs is the place to be'));
 
-assertArraysEqual(letterPositions("hello").e, [1]);
+module.exports = letterPositions;
